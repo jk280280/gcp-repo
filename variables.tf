bfedc1d8639e1213fb7753aeef60a64031e37caf
jk280280 <38111277+jk280280@users.noreply.github.com>
2024-08-27 15:46:43 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "The region for the resources"
+  type        = string
+}
+
+variable "zone" {
+  description = "The zone for the instance group"
+  type        = string
+}
